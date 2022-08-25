5cf54015eeb72ec2a14b3341ae369738086351bf
iknoorsinghchuni <100800884+iknoorsinghchuni@users.noreply.github.com>
2022-08-25 05:46:31 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ npm install
 # For the love of all that is good, don't use this in production
 # This is only for a demonstration of how things work behind the scene
 
-ssh vagrant@192.168.33.11 'sudo mkdir -p /app; sudo chown -R devops. /app'
+ssh devops@192.168.1.144 'sudo mkdir -p /app; sudo chown -R devops. /app'
 rsync -avz ./ devops@192.168.1.144:/app/
 ssh devops@192.168.1.144 "sudo systemctl restart nodeapp"
